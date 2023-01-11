40f4b8c3667e2c8ebab7cc15e30f9eadf64c3088
parksiwoong <tldnd5656@naver.com>
2023-01-11 22:15:48 +0900

    코딩테스트 문제

---
+++ src/Array/등수구하기_8.java
@@ -0,0 +1,10 @@
+package Array;
+/** 등수구하기
+ *
+ * N명의 학생의 국어점수가 입력되면 각 학생의 등수를 입력된 순서대로 출력하는 프로그램을 작정하세요.
+    같은 점수가 입력될 경우*/
+public class 등수구하기_8 {
+    public static void main(String[] args) {
+
+    }
+}
