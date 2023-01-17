f73e148bb5500b7ef8d3dfedb573b864cd278c25
parksiwoong <tldnd5656@naver.com>
2023-01-17 23:45:31 +0900

    [HashMap,treeSet] 2. 아나그램

--- src/HashMapAndTreeSet/아나그램.java
+++ src/HashMapAndTreeSet/아나그램.java
@@ -1,4 +1,8 @@
 package HashMapAndTreeSet;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
 /** 아나그램(해쉬)
  *
  * @설명: Anagram이란 두 문자열이 알파벳의 나열 순서를 다르지만 그 구성이 일치하면 두 단어는 아나그램이라고 합니다.
@@ -17,4 +21,24 @@ package HashMapAndTreeSet;
  *
  * */
 public class 아나그램 {
+    public String solution(String s1, String s2){
+        String answer = "YES";
+        HashMap<Character, Integer> map =new HashMap<>();
+        for(char x : s1.toCharArray()){
+            map.put(x, map.getOrDefault(x, 0)+1);
+        }
+        for(char x : s2.toCharArray()){
+            //키가 존재하는지 보는거 ( !로 존재하지않을때 확인하는거
+            if(!map.containsKey(x) || map.get(x)==0) return "NO";
+            map.put(x, map.get(x)-1);
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        아나그램 T = new 아나그램();
+        Scanner kb = new Scanner(System.in);
+        String a=kb.next();
+        String b=kb.next();
+        System.out.println(T.solution(a,b));
+    }
 }
