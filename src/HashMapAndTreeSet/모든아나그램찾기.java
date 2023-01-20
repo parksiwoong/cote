bf09879e28c317737382b26a0af12d8dfef7176c
parksiwoong <tldnd5656@naver.com>
2023-01-20 23:30:36 +0900

    [HashMap,treeSet] 4. 모든아나그램찾기

--- src/HashMapAndTreeSet/모든아나그램찾기.java
+++ src/HashMapAndTreeSet/모든아나그램찾기.java
@@ -21,6 +21,23 @@ public class 모든아나그램찾기 {
         int answer=0;
         HashMap<Character, Integer> am = new HashMap<>();
         HashMap<Character, Integer> bm = new HashMap<>();
+        for(char x : b.toCharArray()){
+            bm.put(x, bm.getOrDefault(x, 0)+1);
+        }
+        //길이가 3이면 L은 2가  됨
+        int L = b.length()-1;
+        for(int i=0; i<L; i++){
+            am.put(a.charAt(i), am.getOrDefault(a.charAt(i),0)-1);
+        }
+        int lt=0;
+        for(int rt = L; rt<a.length(); rt++){
+            am.put(a.charAt(rt), am.getOrDefault(a.charAt(lt), 0)+1);
+            if(am.equals(bm)){answer++;}
+            am.put(a.charAt(lt), am.remove(a.charAt(lt)));
+            //0일땐 삭제
+            if(am.get(a.charAt(lt))==0) am.remove(a.charAt(lt));
+            lt++;
+        }
         return answer;
     }
     public static void main(String[] args) {
