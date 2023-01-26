a80f7708a0022c9b6c9682db9c726d510607244e
parksiwoong <tldnd5656@naver.com>
2023-01-26 21:29:19 +0900

    [Stack, Queue] 1. 올바른 괄호

--- src/StackQueue/올바른괄호.java
+++ src/StackQueue/올바른괄호.java
@@ -1,5 +1,8 @@
 package StackQueue;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 /** 올바른 괄호
  * 괄호가 입력되면 올바른 괄호이면 "YES" , 올바르지 않으면 "NO"를 출력합니다.
  * (())() 이것은 괄호의 쌍이 올바르게 위치하는 거지만, (()())))은 올바른 괄호가 아니다.
@@ -10,8 +13,25 @@ package StackQueue;
  * @출렬예제: NO
  * */
 public class 올바른괄호 {
-
+    public String solution(String str){
+        String answer="YES";
+        Stack<Character> stack = new Stack<>();
+        for(char x : str.toCharArray()){
+            if(x=='(') stack.push(x);
+            else {
+                //여는괄호에만 스텍에 넣고 그외엔 스텍안이 비어있냐는걸 묻는것
+                if(stack.isEmpty()) return "NO";
+                stack.pop(); //제일 상단(마지막)에 있는거 꺼내는거
+            }
+        }
+        //스텍에 뭔가 남아있으면
+        if(!stack.isEmpty()) return "NO";
+        return answer;
+    }
     public static void main(String[] args) {
-
+        올바른괄호 T = new 올바른괄호();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        System.out.println(T.solution(str));
     }
 }
